65693b4c27e6211883dadc0dca8323b8a9efafec
psahx <ovals.coney_5t@icloud.com>
2025-04-26 12:58:20 -0700

    Update Interface_IMDB_mod.js

--- Interface_IMDB_mod.js
+++ Interface_IMDB_mod.js
@@ -1,4 +1,4 @@
-// == Main Module with TMDB + IMDB + KP Rating Display ==
+// == Main Module with IMDB + TMDB + KP Rating Display (Reordered & Showing 0.0) ==
 (function () {
     'use strict';
 
@@ -16,45 +16,36 @@
       var loaded = {};
 
       this.create = function () {
-        // Original 'create' method
+        // Original 'create' method - UNCHANGED
         html = $("<div class=\"new-interface-info\">\n            <div class=\"new-interface-info__body\">\n                <div class=\"new-interface-info__head\"></div>\n                <div class=\"new-interface-info__title\"></div>\n                <div class=\"new-interface-info__details\"></div>\n                <div class=\"new-interface-info__description\"></div>\n            </div>\n        </div>");
       };
 
       this.update = function (data) {
-        var _this = this; // Capture 'this'
-
-        // Original 'update' method PART 1
+        // Original 'update' method + fetcher call - UNCHANGED from last step
+        var _this = this;
         html.find('.new-interface-info__head,.new-interface-info__details').text('---');
         html.find('.new-interface-info__title').text(data.title);
         html.find('.new-interface-info__description').text(data.overview || Lampa.Lang.translate('full_notext'));
         Lampa.Background.change(Lampa.Api.img(data.backdrop_path, 'w200'));
-        // End Original PART 1
 
-
-        // Minimal fetcher call (Unchanged from previous step)
         if (window.ExternalRatingsFetcher && typeof window.ExternalRatingsFetcher.fetch === 'function' && data.id && !externalRatingsPending[data.id] && !externalRatingsCache[data.id]) {
              externalRatingsPending[data.id] = true;
              window.ExternalRatingsFetcher.fetch(data, function(externalRatingsResult) {
-                 externalRatingsCache[data.id] = externalRatingsResult; // Store result
-                 delete externalRatingsPending[data.id]; // Clear pending flag
-
-                 // Re-draw ONLY if TMDB data is already loaded
+                 externalRatingsCache[data.id] = externalRatingsResult;
+                 delete externalRatingsPending[data.id];
                  var tmdb_url = Lampa.TMDB.api((data.name ? 'tv' : 'movie') + '/' + data.id + '?api_key=' + Lampa.TMDB.key() + '&append_to_response=content_ratings,release_dates&language=' + Lampa.Storage.get('language'));
                  if (loaded[tmdb_url]) {
                      _this.draw(loaded[tmdb_url]);
                  }
              });
         }
-
-
-        // Original 'update' method PART 2
         this.load(data);
-        // End Original PART 2
       };
 
       this.draw = function (data) {
         // Original 'draw' method variables
         var create = ((data.release_date || data.first_air_date || '0000') + '').slice(0, 4);
+        // Calculate TMDB vote - will default to '0.0' if data.vote_average is missing/0
         var vote = parseFloat((data.vote_average || 0) + '').toFixed(1);
         var head = [];
         var details = []; // Original details array
@@ -65,36 +56,33 @@
         if (create !== '0000') head.push('<span>' + create + '</span>');
         if (countries.length > 0) head.push(countries.join(', '));
 
-        // Original details population
-        // 1. Add original TMDB rating
-        if (vote > 0) {
-            details.push('<div class="full-start__rate"><div>' + vote + '</div><div>TMDB</div></div>');
-        }
-
-        // 2. Check cache and add IMDB rating (Unchanged from previous step)
+        // --- **MODIFIED RATING DISPLAY LOGIC** ---
+        // Get external ratings from cache (might be undefined if fetch not done/failed)
         var external = externalRatingsCache[data.id];
-        if (external && external.imdb > 0) {
-            details.push('<div class="full-start__rate"><div>' + parseFloat(external.imdb).toFixed(1) + '</div><div>IMDB</div></div>');
-        }
 
-        // 3. **ADDED**: Check cache and add KP rating
-        if (external && external.kp > 0) {
-             // Use the fetched KP number and "KP" text, same structure
-             details.push('<div class="full-start__rate"><div>' + parseFloat(external.kp).toFixed(1) + '</div><div>KP</div></div>');
-        }
-        // *** END ADDED ***
+        // 1. IMDB Rating (Always display)
+        var imdbRating = external ? parseFloat(external.imdb || 0).toFixed(1) : '0.0';
+        details.push('<div class="full-start__rate"><div>' + imdbRating + '</div><div>IMDB</div></div>');
+
+        // 2. TMDB Rating (Always display)
+        // 'vote' is already calculated above, defaulting to '0.0'
+        details.push('<div class="full-start__rate"><div>' + vote + '</div><div>TMDB</div></div>');
+
+        // 3. KP Rating (Always display)
+        var kpRating = external ? parseFloat(external.kp || 0).toFixed(1) : '0.0';
+        details.push('<div class="full-start__rate"><div>' + kpRating + '</div><div>KP</div></div>');
+        // --- **END MODIFIED RATING DISPLAY LOGIC** ---
 
 
-        // Add other original details (genres, runtime, pg)
+        // Add other original details (genres, runtime, pg) - UNCHANGED
         if (data.genres && data.genres.length > 0) details.push(data.genres.map(function (item) {
           return Lampa.Utils.capitalizeFirstLetter(item.name);
         }).join(' | '));
         if (data.runtime) details.push(Lampa.Utils.secondsToTime(data.runtime * 60, true));
         if (pg) details.push('<span class="full-start__pg" style="font-size: 0.9em;">' + pg + '</span>');
 
-        // Original HTML update
+        // Original HTML update - UNCHANGED
         html.find('.new-interface-info__head').empty().append(head.join(', '));
-        // Use original details join logic
         html.find('.new-interface-info__details').html(details.join('<span class="new-interface-info__split">&#9679;</span>'));
       };
 
@@ -114,18 +102,12 @@
         }, 300);
       };
 
-      // Original 'render', 'empty' methods - UNCHANGED
+      // Original 'render', 'empty', 'destroy' methods - UNCHANGED (except destroy clears cache)
       this.render = function () { return html; };
       this.empty = function () {};
-
       this.destroy = function () {
-        // Original destroy
-        html.remove();
-        loaded = {};
-        html = null;
-        // Clear global cache (Unchanged from previous step)
-        externalRatingsCache = {};
-        externalRatingsPending = {};
+        html.remove(); loaded = {}; html = null;
+        externalRatingsCache = {}; externalRatingsPending = {}; // Keep cache clear
       };
     }
 
@@ -148,55 +130,26 @@
         var background_timer;
 
         this.create = function () {};
-        this.empty = function () { /* Original empty code */
-            var button; if (object.source == 'tmdb') { button = $('<div class="empty__footer"><div class="simple-button selector">' + Lampa.Lang.translate('change_source_on_cub') + '</div></div>'); button.find('.selector').on('hover:enter', function () { Lampa.Storage.set('source', 'cub'); Lampa.Activity.replace({ source: 'cub' }); }); } var empty = new Lampa.Empty(); html.append(empty.render(button)); this.start = empty.start; this.activity.loader(false); this.activity.toggle();
-        };
-        this.loadNext = function () { /* Original loadNext code */
-             var _this = this; if (this.next && !this.next_wait && items.length) { this.next_wait = true; this.next(function (new_data) { _this.next_wait = false; new_data.forEach(_this.append.bind(_this)); Lampa.Layer.visible(items[active + 1].render(true)); }, function () { _this.next_wait = false; }); }
-        };
+        this.empty = function () { /* Original empty code */ var button; if (object.source == 'tmdb') { button = $('<div class="empty__footer"><div class="simple-button selector">' + Lampa.Lang.translate('change_source_on_cub') + '</div></div>'); button.find('.selector').on('hover:enter', function () { Lampa.Storage.set('source', 'cub'); Lampa.Activity.replace({ source: 'cub' }); }); } var empty = new Lampa.Empty(); html.append(empty.render(button)); this.start = empty.start; this.activity.loader(false); this.activity.toggle(); };
+        this.loadNext = function () { /* Original loadNext code */ var _this = this; if (this.next && !this.next_wait && items.length) { this.next_wait = true; this.next(function (new_data) { _this.next_wait = false; new_data.forEach(_this.append.bind(_this)); Lampa.Layer.visible(items[active + 1].render(true)); }, function () { _this.next_wait = false; }); } };
         this.push = function () {};
-        this.build = function (data) { // Original build code
-            var _this2 = this;
-            lezydata = data;
-            info = new create(object); // Instantiates modified 'create'
-            info.create();
-            scroll.minus(info.render());
-            data.slice(0, viewall ? data.length : 2).forEach(this.append.bind(this));
-            html.append(info.render());
-            html.append(scroll.render());
-            if (newlampa) { /* Original newlampa code */
-                 Lampa.Layer.update(html); Lampa.Layer.visible(scroll.render(true)); scroll.onEnd = this.loadNext.bind(this); scroll.onWheel = function (step) { if (!Lampa.Controller.own(_this2)) _this2.start(); if (step > 0) _this2.down(); else if (active > 0) _this2.up(); };
-            }
-            // Trigger update for first item
-            if (items.length > 0 && items[0] && items[0].data) {
-                 active = 0; info.update(items[active].data); this.background(items[active].data);
-            }
-            this.activity.loader(false); this.activity.toggle();
-        };
-        this.background = function (elem) { /* Original background code */
-             if (!elem || !elem.backdrop_path) return; var new_background = Lampa.Api.img(elem.backdrop_path, 'w1280'); clearTimeout(background_timer); if (new_background == background_last) return; background_timer = setTimeout(function () { background_img.removeClass('loaded'); background_img[0].onload = function () { background_img.addClass('loaded'); }; background_img[0].onerror = function () { background_img.removeClass('loaded'); }; background_last = new_background; setTimeout(function () { if (background_img[0]) background_img[0].src = background_last; }, 300); }, 1000);
-        };
-        this.append = function (element) { // Original append code
-            if (element.ready) return; var _this3 = this; element.ready = true; var item = new Lampa.InteractionLine(element, { url: element.url, card_small: true, cardClass: element.cardClass, genres: object.genres, object: object, card_wide: true, nomore: element.nomore }); item.create(); item.onDown = this.down.bind(this); item.onUp = this.up.bind(this); item.onBack = this.back.bind(this); item.onToggle = function () { active = items.indexOf(item); }; if (this.onMore) item.onMore = this.onMore.bind(this); item.onFocus = function (elem) { info.update(elem); _this3.background(elem); }; item.onHover = function (elem) { info.update(elem); _this3.background(elem); }; item.onFocusMore = info.empty.bind(info); scroll.append(item.render()); items.push(item);
-        };
+        this.build = function (data) { /* Original build code */ var _this2 = this; lezydata = data; info = new create(object); info.create(); scroll.minus(info.render()); data.slice(0, viewall ? data.length : 2).forEach(this.append.bind(this)); html.append(info.render()); html.append(scroll.render()); if (newlampa) { /* Original newlampa code */ Lampa.Layer.update(html); Lampa.Layer.visible(scroll.render(true)); scroll.onEnd = this.loadNext.bind(this); scroll.onWheel = function (step) { if (!Lampa.Controller.own(_this2)) _this2.start(); if (step > 0) _this2.down(); else if (active > 0) _this2.up(); }; } if (items.length > 0 && items[0] && items[0].data) { active = 0; info.update(items[active].data); this.background(items[active].data); } this.activity.loader(false); this.activity.toggle(); };
+        this.background = function (elem) { /* Original background code */ if (!elem || !elem.backdrop_path) return; var new_background = Lampa.Api.img(elem.backdrop_path, 'w1280'); clearTimeout(background_timer); if (new_background == background_last) return; background_timer = setTimeout(function () { background_img.removeClass('loaded'); background_img[0].onload = function () { background_img.addClass('loaded'); }; background_img[0].onerror = function () { background_img.removeClass('loaded'); }; background_last = new_background; setTimeout(function () { if (background_img[0]) background_img[0].src = background_last; }, 300); }, 1000); };
+        this.append = function (element) { /* Original append code */ if (element.ready) return; var _this3 = this; element.ready = true; var item = new Lampa.InteractionLine(element, { url: element.url, card_small: true, cardClass: element.cardClass, genres: object.genres, object: object, card_wide: true, nomore: element.nomore }); item.create(); item.onDown = this.down.bind(this); item.onUp = this.up.bind(this); item.onBack = this.back.bind(this); item.onToggle = function () { active = items.indexOf(item); }; if (this.onMore) item.onMore = this.onMore.bind(this); item.onFocus = function (elem) { info.update(elem); _this3.background(elem); }; item.onHover = function (elem) { info.update(elem); _this3.background(elem); }; item.onFocusMore = info.empty.bind(info); scroll.append(item.render()); items.push(item); };
         this.back = function () { Lampa.Activity.backward(); };
         this.down = function () { active++; active = Math.min(active, items.length - 1); if (!viewall && lezydata) lezydata.slice(0, active + 2).forEach(this.append.bind(this)); items[active].toggle(); scroll.update(items[active].render()); };
         this.up = function () { active--; if (active < 0) { active = 0; Lampa.Controller.toggle('head'); } else { items[active].toggle(); scroll.update(items[active].render()); } };
-        this.start = function () { /* Original start code */
-             var _this4 = this; Lampa.Controller.add('content', { link: this, toggle: function toggle() { if (_this4.activity.canRefresh()) return false; if (items.length) { items[active].toggle(); } }, update: function update() {}, left: function left() { if (Navigator.canmove('left')) Navigator.move('left'); else Lampa.Controller.toggle('menu'); }, right: function right() { Navigator.move('right'); }, up: function up() { if (Navigator.canmove('up')) Navigator.move('up'); else Lampa.Controller.toggle('head'); }, down: function down() { if (Navigator.canmove('down')) Navigator.move('down'); }, back: this.back }); Lampa.Controller.toggle('content');
-        };
+        this.start = function () { /* Original start code */ var _this4 = this; Lampa.Controller.add('content', { link: this, toggle: function toggle() { if (_this4.activity.canRefresh()) return false; if (items.length) { items[active].toggle(); } }, update: function update() {}, left: function left() { if (Navigator.canmove('left')) Navigator.move('left'); else Lampa.Controller.toggle('menu'); }, right: function right() { Navigator.move('right'); }, up: function up() { if (Navigator.canmove('up')) Navigator.move('up'); else Lampa.Controller.toggle('head'); }, down: function down() { if (Navigator.canmove('down')) Navigator.move('down'); }, back: this.back }); Lampa.Controller.toggle('content'); };
         this.refresh = function () { this.activity.loader(true); this.activity.need_refresh = true; };
         this.pause = function () {};
         this.stop = function () {};
         this.render = function () { return html; };
-        this.destroy = function () { /* Original destroy code */
-             clearTimeout(background_timer); network.clear(); Lampa.Arrays.destroy(items); scroll.destroy(); if (info) info.destroy(); /* Calls modified destroy */ if (html) html.remove(); items = null; network = null; lezydata = null; info = null; html = null;
-        };
+        this.destroy = function () { /* Original destroy code */ clearTimeout(background_timer); network.clear(); Lampa.Arrays.destroy(items); scroll.destroy(); if (info) info.destroy(); /* Calls modified destroy */ if (html) html.remove(); items = null; network = null; lezydata = null; info = null; html = null; };
     }
 
 
     // --- Plugin Initialization Logic ---
-    // ORIGINAL FUNCTION - UNCHANGED From previous step
+    // ORIGINAL FUNCTION - UNCHANGED from last step
     function startPlugin() {
         if (!window.Lampa || !Lampa.Utils || !Lampa.Lang) {
             console.error("NewInterface Minimal: Missing Lampa components");
