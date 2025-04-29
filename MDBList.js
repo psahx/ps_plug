89e0773370a9f6e9295fd398c71fb93f97c3e787
psahx <ovals.coney_5t@icloud.com>
2025-04-28 23:32:04 -0700

    Update MDBList.js
    Design tweaks

--- MDBList.js
+++ MDBList.js
@@ -425,7 +425,7 @@
             <style data-id="${style_id}">
             /* Base styles... (kept from pivot point script) */
             .new-interface .card--small.card--wide { width: 18.3em; }
-            .new-interface-info { position: relative; padding: 1.5em; height: 22.5em; } /* original was 24em*/
+            .new-interface-info { position: relative; padding: 1.5em; height: 24em; } /* original was 24em*/
             /* ... rest of base styles identical to pivot script ... */
             .new-interface-info__body { width: 80%; padding-top: 1.1em; }
             .new-interface-info__head { color: rgba(255, 255, 255, 0.6); margin-bottom: 1em; font-size: 1.3em; min-height: 1em; }
@@ -434,7 +434,7 @@
             /* .new-interface-info__details { margin-bottom: 1.6em; display: flex; align-items: center; flex-wrap: wrap; min-height: 1.9em; font-size: 1.1em; } */
                         
             .new-interface-info__details {
-                margin-bottom: 1.6em;
+                margin-bottom: 1em; 
                 display: block;
                 min-height: 1.9em;
                 font-size: 1.1em;
