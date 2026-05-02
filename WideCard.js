aea962bf2993a2b0bf5d6ea1cdb0d499c95016dc
psahx <ovals.coney_5t@icloud.com>
2026-05-01 21:09:08 -0700

    Update WideCard.js
    V11

--- WideCard.js
+++ WideCard.js
@@ -1,4 +1,4 @@
-// == Lampa Homepage Wide Card DOM Proof V10 ==
+// == Lampa Hover Trigger Proof V11 ==
 (function () {
     'use strict';
 
@@ -8,19 +8,14 @@
         if (!path) return null;
         var file = path.split('/').pop();
         file = file.split('?')[0].split('#')[0]; 
-        if (file && (file.indexOf('.jpg') > -1 || file.indexOf('.png') > -1)) {
-            return file;
-        }
+        if (file && (file.indexOf('.jpg') > -1 || file.indexOf('.png') > -1)) return file;
         return null;
     }
 
     function extractMoviesSafe(data) {
         if (!data) return;
-        
         if (Array.isArray(data)) {
-            data.forEach(function(item) {
-                extractMoviesSafe(item);
-            });
+            data.forEach(function(item) { extractMoviesSafe(item); });
         } else if (typeof data === 'object') {
             if (data.id && (data.poster_path || data.backdrop_path)) {
                 var pName = getSafeFilename(data.poster_path);
@@ -28,7 +23,6 @@
                 if (pName) window.lampa_movie_dict[pName] = data;
                 if (bName) window.lampa_movie_dict[bName] = data;
             }
-            
             if (data.results) extractMoviesSafe(data.results);
             if (data.items) extractMoviesSafe(data.items);
             if (data.movies) extractMoviesSafe(data.movies);
@@ -36,9 +30,8 @@
         }
     }
 
-    // Hook 1: Catch initial loads from the Local Cache
-    function hookLampaApi() {
-        if (!window.Lampa || !Lampa.Api) return false;
+    // Catch the instant cache for the first 6 rows
+    if (window.Lampa && Lampa.Api) {
         ['main', 'list', 'get', 'search'].forEach(function(method) {
             if (Lampa.Api[method] && !Lampa.Api[method]._hooked) {
                 var original = Lampa.Api[method];
@@ -51,90 +44,77 @@
                 Lampa.Api[method]._hooked = true;
             }
         });
-        return true;
     }
 
-    // Hook 2: Catch lazy-loaded rows from the Network while scrolling
-    function hookLampaNetwork() {
-        if (window.Lampa && Lampa.Reguest && !Lampa.Reguest._hooked) {
-            var orig_silent = Lampa.Reguest.prototype.silent;
-            Lampa.Reguest.prototype.silent = function(url, onsuccess, onerror) {
-                var new_onsuccess = function(data) {
-                    try { extractMoviesSafe(data); } catch (e) {}
-                    if (onsuccess) onsuccess(data);
-                };
-                return orig_silent.call(this, url, new_onsuccess, onerror);
-            };
+    // The Master Layout Changer
+    function applyWideStyle(card) {
+        if (card.hasClass('card--wide')) return;
+        
+        var imgElement = card.find('.card__img');
+        var currentSrc = imgElement.attr('src') || imgElement.attr('data-src') || "";
+        if (!currentSrc) return;
 
-            var orig_request = Lampa.Reguest.prototype.request;
-            Lampa.Reguest.prototype.request = function(url, onsuccess, onerror) {
-                var new_onsuccess = function(data) {
-                    try { extractMoviesSafe(data); } catch (e) {}
-                    if (onsuccess) onsuccess(data);
-                };
-                return orig_request.call(this, url, new_onsuccess, onerror);
-            };
-            Lampa.Reguest._hooked = true;
-        }
-    }
+        var filename = getSafeFilename(currentSrc);
+        var movie = window.lampa_movie_dict[filename];
 
-    function proveWideDOM_V10() {
-        setInterval(function() {
-            var activity = window.Lampa && Lampa.Activity ? Lampa.Activity.active() : null;
+        // Make it physically wide
+        card.addClass('card--wide');
+        
+        var titleText = card.find('.card__title').text() || "Unknown";
+        var synopsis = ""; 
+
+        // Scenario A: We have the real data from the dictionary
+        if (movie) {
+            titleText = movie.title || movie.name || titleText;
+            synopsis = movie.overview || "";
+            if (synopsis.length > 115) synopsis = synopsis.substring(0, 115) + '...';
             
-            if (activity && (activity.component === 'main' || activity.component === 'category')) {
-                
-                $('.card:not(.card--wide):visible').each(function() {
-                    var card = $(this);
-                    
-                    var imgElement = card.find('.card__img');
-                    var currentSrc = imgElement.attr('src') || imgElement.attr('data-src') || "";
-                    if (!currentSrc) return;
-
-                    var filename = getSafeFilename(currentSrc);
-                    var movie = window.lampa_movie_dict[filename];
-                    
-                    if (!movie) return;
-
-                    card.addClass('card--wide');
-                    
-                    var targetImage = movie.backdrop_path ? movie.backdrop_path : movie.poster_path;
-                    if (targetImage) {
-                        imgElement.attr('src', Lampa.Api.img(targetImage, 'w780'));
-                        imgElement.css({
-                            'object-fit': 'cover',
-                            'object-position': 'top'
-                        });
-                    }
-                    
-                    var titleText = movie.title || movie.name || "Unknown";
-                    var synopsis = movie.overview || "No description available.";
-                    if (synopsis.length > 115) {
-                        synopsis = synopsis.substring(0, 115) + '...';
-                    }
-                    
-                    card.find('.card__title, .card__age').remove();
-                    
-                    var promoHtml = $(
-                        '<div class="card__promo">' + 
-                            '<div class="card__promo-title">' + titleText + '</div>' + 
-                            '<div class="card__promo-text">' + synopsis + '</div>' + 
-                        '</div>'
-                    );
-                    
-                    card.find('.card__view').append(promoHtml);
-                });
+            var targetImage = movie.backdrop_path ? movie.backdrop_path : movie.poster_path;
+            if (targetImage) {
+                imgElement.attr('src', Lampa.Api.img(targetImage, 'w780'));
+                imgElement.css({'object-fit': 'cover', 'object-position': 'top'});
             }
-        }, 500); 
+        } 
+        // Scenario B: Missing data fallback (Upgrades DOM, leaves text blank)
+        else {
+            var hdSrc = currentSrc.replace('/w500/', '/w780/').replace('/w342/', '/w780/');
+            imgElement.attr('src', hdSrc);
+            imgElement.css({'object-fit': 'cover', 'object-position': 'center'});
+        }
+
+        card.find('.card__title, .card__age').remove();
+        
+        var promoHtml = $(
+            '<div class="card__promo">' + 
+                '<div class="card__promo-title">' + titleText + '</div>' + 
+                '<div class="card__promo-text">' + synopsis + '</div>' + 
+            '</div>'
+        );
+        
+        card.find('.card__view').append(promoHtml);
     }
 
-    // Deploy both hooks immediately on boot
-    var bootInterval = setInterval(function() {
-        if (hookLampaApi()) {
-            hookLampaNetwork(); // Activate Hook 2 once Lampa is ready
-            clearInterval(bootInterval);
+    // 1. Initial automated sweep (Only applies if data is perfectly matched)
+    setInterval(function() {
+        var activity = window.Lampa && Lampa.Activity ? Lampa.Activity.active() : null;
+        if (activity && (activity.component === 'main' || activity.component === 'category')) {
+            $('.card:not(.card--wide):visible').each(function() {
+                var card = $(this);
+                var filename = getSafeFilename(card.find('.card__img').attr('src'));
+                // Auto-convert ONLY the first rows that we have guaranteed data for
+                if (window.lampa_movie_dict[filename]) {
+                    applyWideStyle(card);
+                }
+            });
         }
-    }, 50);
+    }, 500);
 
-    setTimeout(proveWideDOM_V10, 500);
+    // 2. The User's Hover Trigger (Catches everything else)
+    $(document).on('mouseenter focus hover:focus', '.card:not(.card--wide)', function() {
+        var activity = window.Lampa && Lampa.Activity ? Lampa.Activity.active() : null;
+        if (activity && (activity.component === 'main' || activity.component === 'category')) {
+            applyWideStyle($(this));
+        }
+    });
+
 })();
